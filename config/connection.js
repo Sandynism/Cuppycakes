7c662042192903b82c665a9619c47be603a028a0
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-09-21 09:14:31 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Mysql Dependencies.
 let mysql = require("mysql")
+let connection 
 
-let connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "cupcake_db"
-});
+})
+}
 
 // Make connection.
 connection.connect(function(err) {
